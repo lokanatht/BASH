9d97e5a8111060ad57a43e2b690a2ece25b623f2
root <root@server2.lokilabs.ai>
2021-01-25 11:53:53 +0530

    print greater value

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BASH
 bash projects
+1	cond1.sh		checking if condition
+2	hai.txt			text file write in github site
---
+++ great.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+a=$1
+b=$2
+c=$3
+d=$4
+
+if [ $a -gt $b -a $a -gt $c -a $a -gt $d ]
+then 
+    echo " A value $a is greater "
+elif [ $b -gt $c -a $b -gt $d ]
+then 
+    echo " B value $b is greater "
+elif [ $c -gt $d ]
+then 
+    echo " C value $c is greater "
+else
+    echo " D value $d is greater "
+fi
